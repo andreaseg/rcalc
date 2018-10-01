e11ffdfedb0f8c441e60fc7671346fcb878c2f99
Andreas Grønlien <andreas@gronlien.no>
2018-10-01 12:24:05 +0200

    Added basic error recovery

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,38 @@ mod scanner;
 
 use std::io;
 use std::io::prelude::*;
+use std::panic;
 
 fn main() {
     loop {
         let stdin = io::stdin();
         for line in stdin.lock().lines() {
             match line {
-                Ok(l) => {
-                    if l == "exit" {
+                Ok(line) => {
+                    if line == "exit" {
                         return;
                     }
-                    let t = scanner::tokenize(&l);
-                    let ast = parser::parse(t);
-                    let result = eval::eval(ast);
+                    let tokens = match panic::catch_unwind(|| scanner::tokenize(&line)) {
+                        Ok(ok) => ok,
+                        Err(e) => {
+                            print!("Error parsing tokens: {:?}", e);
+                            continue;
+                        }
+                    };
+                    let ast = match panic::catch_unwind(|| parser::parse(tokens)) {
+                        Ok(ok) => ok,
+                        Err(e) => {
+                            print!("Error parsing AST: {:?}", e);
+                            continue;
+                        }
+                    };
+                    let result = match panic::catch_unwind(|| eval::eval(ast)) {
+                        Ok(ok) => ok,
+                        Err(e) => {
+                            print!("Error parsing AST: {:?}", e);
+                            continue;
+                        }
+                    };
                     println!("{:?}", result);
                 }
                 Err(e) => panic!("IO error {}", e),
